93b31a640548b98449276987ed1937184a239f3e
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-07-15 13:54:27 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -12,23 +14,265 @@ import io
 import plotly.express as px
 import plotly.graph_objs as go
 
+st.title('Bienvenue sur Octroi de crédit !')
+
+# loading the trained model
+#with open(r'C:\Users\Catherine\Credit\classifier.pkl', 'rb') 
+
+current_path = os.getcwd()
+credit_path = os.path.join(current_path, 'classifier.pkl')
+with open(credit_path, 'rb') as handle:
+    model = pickle.load(handle)
+
+
+########################################################
+# Loading images to the website
+########################################################
+image = Image.open("images/credit.jpg")
+
+@st.cache()
+def prediction(X):
+    prediction = model.predict(X)
+    #if prediction == 0:
+    #    pred = 'Approved'
+    #else:
+    #    pred = 'Rejected'
+    return prediction 
+
+######################################
+# Feature Selection Code
+######################################
+def impPlot(imp, name):
+    figure = px.bar(imp,
+                    x=imp.values,
+                    y=imp.keys(), labels = {'x':'Importance Value', 'index':'Columns'},
+                    text=np.round(imp.values, 2),
+                    title=name + ' Feature Selection Plot',
+                    width=1000, height=600)
+    figure.update_layout({
+        'plot_bgcolor': 'rgba(0, 0, 0, 0)',
+        'paper_bgcolor': 'rgba(0, 0, 0, 0)',
+    })
+    st.plotly_chart(figure)
+    
+    
+
 def main():
-    # １
-    st.sidebar.title("test_streamlit")
-    st.session_state["file"]=st.sidebar.file_uploader("upload", on_change=change_page)
+    def chargement_data(path):
+        dataframe = pd.read_csv(path)
+        liste_id = dataframe['SK_ID_CURR'].tolist()
+        return dataframe, liste_id
+        
+    
+    st.subheader("Prédictions de scoring client et positionnement dans l'ensemble des clients")
 
-def change_page():
-    # 
-    st.session_state["page_control"]=1
+    examples_file = 'df1.csv'
+    dataframe, liste_id = chargement_data(examples_file)
 
-def next_page():
-    # ２
-    st.sidebar.title("next_page")
-    st.markdown("## next_page")
+    st.sidebar.image(image)
+    st.sidebar.markdown("🛰️ **Navigation**")
 
-# 
-if ("page_control" in st.session_state and
-   st.session_state["page_control"] == 1):
-    next_page()
+    id_input = st.sidebar.selectbox(
+        'Choisissez le client que vous souhaitez visualiser',
+        liste_id)
+
+    #focus_var = st.sidebar.selectbox('Choisissez la variable de focus',
+    #                                 ['EXT_SOURCE_1',
+    #                                  'EXT_SOURCE_2',
+    #                                  'EXT_SOURCE_3'])
+
+    client_infos = dataframe[dataframe['SK_ID_CURR'] == id_input].drop(
+        ['SK_ID_CURR'], axis=1)
+    #client_infos = client_infos.to_dict('record')[0]
+    client_infos.to_dict(orient = 'records')
+    
+    result =""
+    
+    if st.sidebar.button("Predict"):
+        # correction 07/07 pour déceler les faux positifs
+        X1 = dataframe[dataframe['SK_ID_CURR'] == id_input]    
+        X = X1[['CODE_GENDER', 
+        'AGE',
+        'CNT_CHILDREN', 
+        'DEF_30_CNT_SOCIAL_CIRCLE',
+         'NAME_EDUCATION_TYPE_High education',  
+         'NAME_EDUCATION_TYPE_Low education',  
+         'NAME_EDUCATION_TYPE_Medium education',  
+         'ORGANIZATION_TYPE_Construction',  
+         'ORGANIZATION_TYPE_Electricity',  
+         'ORGANIZATION_TYPE_Government/Industry',  
+         'ORGANIZATION_TYPE_Medicine',  
+         'ORGANIZATION_TYPE_Other/Construction/Agriculture',  
+         'ORGANIZATION_TYPE_School',  
+         'ORGANIZATION_TYPE_Services',  
+         'ORGANIZATION_TYPE_Trade/Business', 
+         'OCCUPATION_TYPE_Accountants/HR staff/Managers', 
+         'OCCUPATION_TYPE_Core/Sales staff',  
+         'OCCUPATION_TYPE_Laborers',  
+         'OCCUPATION_TYPE_Medicine staff',  
+         'OCCUPATION_TYPE_Private service staff' , 
+         'OCCUPATION_TYPE_Tech Staff',
+         'NAME_FAMILY_STATUS',
+          'AMT_INCOME_TOTAL',
+          'INCOME_CREDIT_PERC',
+          'DAYS_EMPLOYED_PERC',
+          'EXT_SOURCE_1',
+          'EXT_SOURCE_2',    
+          'EXT_SOURCE_3']]
+        
+        result = prediction(X)
+        
+        if  result == 1:
+            if int(X1['TARGET']) == 1: 
+                pred = 'Rejected (True Negative)'
+            else:
+                pred = 'Approved (False Negative)'
+        else:
+            if int(X1['TARGET']) == 1:
+                pred = 'Rejected (False Positive)'
+            else:
+                pred = 'Approved (True Positive)'              
+                   
+        st.success('Your loan is {}'.format(pred))
+
+        
+        
+        # SHAP variables locales 
+        st.header("Graphique d'explication")
+        feat_importances = pd.Series(model.feature_importances_, index=X.columns).sort_values(ascending=False)
+        st.subheader('Random Forest Classifier:')
+        impPlot(feat_importances, 'Random Forest Classifier')
+        #trace = go.Bar(x=feat_importances.values,y=feat_importances.keys(),showlegend = True)
+        #layout = go.Layout(title = "Importance des features")
+        #data = [trace]
+        #fig = go.Figure(data=data,layout=layout)
+        #st.plotly_chart(fig)
+                   
+        
+        
+
+        #if focus_var == 'EXT_SOURCE_1':           
+        #   source1 = application[['TARGET', 'EXT_SOURCE_1']]
+        #   source1['SOURCE_BINNED'] = pd.cut(source1['EXT_SOURCE_1'], bins = np.linspace(0.2, 0.8, num = 15))
+        #   ext_source1  = source1.groupby('SOURCE_BINNED').mean()               
+        #   trace = go.Bar(x=ext_source1.index.astype(str),y=ext_source1['TARGET'].values*100,showlegend = True)
+        #   layout = go.Layout(title = "% Faillite par tranches de source1")
+        #   data = [trace]
+        #   fig = go.Figure(data=data,layout=layout)
+        #   st.plotly_chart(fig)
+        
+        
+        
+        #scaler = StandardScaler()
+        #input_data = scaler.fit_transform([[CODE_GENDER,
+        
+             
+    if st.button("Transparence Client"):
+        client = id_input
+        main1(client)
+        
+def main1():      
+            st.subheader("Transparence du client ")    
+# informations du client
+            examples_file = 'application_API.csv'
+            application, liste_id = chargement_data(examples_file)
+            application = application[~((application['EXT_SOURCE_1'].isnull()))]
+            X1 = application[application['SK_ID_CURR'] == id_input]  
+            st.write(X1)
+                    # réalimenter X2 avec les variables saisies
+        # Saisie des informations Client dans X2 pour prédiction nouvelle
+            X2 = X1.copy()
+                
+            AGE = st.slider("AGE", 1, 100, 25)
+            X2['AGE'] = AGE
+        
+            CNT_CHILDREN = st.slider("CNT_CHILDREN", 1, 5, 3)
+            X2['CNT_CHILDREN'] = CNT_CHILDREN
+        
+            DEF_30_CNT_SOCIAL_CIRCLE  = st.slider(" DEF_30_CNT_SOCIAL_CIRCLE, ", 1, 100, 5)
+            X2['DEF_30_CNT_SOCIAL_CIRCLE'] = DEF_30_CNT_SOCIAL_CIRCLE
+        
+            NAME_EDUCATION_TYPE = st.selectbox("NAME_EDUCATION_TYPE",options=['Low education','Medium education','High education'])        
+        
+            ORGANIZATION_TYPE = st.selectbox("ORGANIZATION_TYPE",options=['Construction', 'Electricity', 'Government/Industry',
+                                                                          'Medicine', 
+                                                                      'Other/Construction/Agriculture', 'School', 'Services', 
+                                                                      'Trade/Business'])    
+        
+            OCCUPATION_TYPE = st.selectbox("OCCUPATION_TYPE",options=['Accountants/HR staff/Managers','Core/Sales staff','Laborers',
+                                                                  'Medicine staff','Private service staff' , 'Tech Staff'])    
+        
+            NAME_FAMILY_STATUS = st.selectbox("NAME_FAMILY_STATUS",options=['Single' , 'Married'])        
+        
+            AMT_INCOME_TOTAL = st.slider("AMT_INCOME_TOTAL", 1, 500000, 220000)
+            INCOME_CREDIT_PERC = st.slider("INCOME_CREDIT_PERC", 1, 100, 220000)
+            DAYS_EMPLOYED_PERC = st.slider("DAYS_EMPLOYED_PERC", 1, 100, 65)
+            EXT_SOURCE_1 = st.slider("EXT_SOURCE_1", 1, 100,35)
+            EXT_SOURCE_2 = st.slider("EXT_SOURCE_2", 1, 100, 35)
+            EXT_SOURCE_3 = st.slider("EXT_SOURCE_3", 1, 100, 35)  
+        
+        # Encoding for prediction
+        #CODE_GENDER = 0 if  CODE_GENDER == 'M' else 1
+        
+            NAME_EDUCATION_TYPE_Low_education , NAME_EDUCATION_TYPE_Medium_education , NAME_EDUCATION_TYPE_High_education = 0,0,0
+            if NAME_EDUCATION_TYPE == 'Low education':
+              NAME_EDUCATION_TYPE_Low_education = 1
+            elif NAME_EDUCATION_TYPE == 'Medium education':
+              NAME_EDUCATION_TYPE_Medium_education = 1
+            else:
+              NAME_EDUCATION_TYPE_High_education = 1
+            X2['NAME_EDUCATION_TYPE'] = NAME_EDUCATION_TYPE    
+            
+            ORGANIZATION_TYPE_Construction, ORGANIZATION_TYPE_Electricity, ORGANIZATION_TYPE_Government_Industry = 0,0,0
+            ORGANIZATION_TYPE_Medicine, ORGANIZATION_TYPE_Other_Construction_Agriculture, ORGANIZATION_TYPE_School = 0,0,0
+            ORGANIZATION_TYPE_Services, ORGANIZATION_TYPE_Trade_Business = 0,0 
+            if ORGANIZATION_TYPE == 'Construction':
+               ORGANIZATION_TYPE_Construction = 1
+            elif ORGANIZATION_TYPE == 'Electricity':
+               ORGANIZATION_TYPE_Electricity = 1
+            elif ORGANIZATION_TYPE ==  'Government/Industry':   
+               ORGANIZATION_TYPE_Government_Industry = 1 
+            elif  ORGANIZATION_TYPE == 'Medicine':    
+               ORGANIZATION_TYPE_Medicine = 1
+            elif  ORGANIZATION_TYPE == 'Other/Construction/Agriculture':
+               ORGANIZATION_TYPE_Other_Construction_Agriculture = 1
+            elif  ORGANIZATION_TYPE ==  'School': 
+               ORGANIZATION_TYPE_School = 1
+            elif ORGANIZATION_TYPE == 'Services':
+               ORGANIZATION_TYPE_Services = 1
+            elif ORGANIZATION_TYPE == 'Trade/Business':
+               ORGANIZATION_TYPE_Trade_Business = 1
+            X2['ORGANIZATION_TYPE'] = ORGANIZATION_TYPE    
+            
+            OCCUPATION_TYPE_Accountants_HR_staff_Managers, OCCUPATION_TYPE_Core_Sales_staff, OCCUPATION_TYPE_Laborers = 0,0,0  
+            OCCUPATION_TYPE_Medicine_staff, OCCUPATION_TYPE_Private_service_staff, OCCUPATION_TYPE_Tech_Staff = 0,0,0
+            if OCCUPATION_TYPE == 'Accountants/HR staff/Managers':
+               OCCUPATION_TYPE_Accountants_HR_staff_Managers = 1
+            elif OCCUPATION_TYPE == 'Core/Sales staff':
+               OCCUPATION_TYPE_Core_Sales_staff = 1
+            elif OCCUPATION_TYPE == 'Laborers':
+               OCCUPATION_TYPE_Laborers = 1
+            elif OCCUPATION_TYPE == 'Medicine staff':
+               OCCUPATION_TYPE_Medicine_staff = 1
+            elif OCCUPATION_TYPE == 'Private service staff':
+               OCCUPATION_TYPE_Private_service_staff = 1 
+            elif OCCUPATION_TYPE ==  'Tech Staff':
+               OCCUPATION_TYPE_Tech_Staff = 1
+            X2['OCCUPATION_TYPE'] = OCCUPATION_TYPE
+        
+            NAME_FAMILY_STATUS = 0 if  NAME_FAMILY_STATUS == 'Single' else 1
+            X2['NAME_FAMILY_STATUS'] = NAME_FAMILY_STATUS
+            transparence = prediction(X2)
+            predict_probability = model.predict_proba(X2)
+        
+            if transparence == 1:
+              st.subheader('Le client {} aurait une faillite avec une probabilité de {}%'.format
+                            (id_input ,round(predict_probability[0][1]*100 , 3)))
+            else:
+              st.subheader('Le client {} aurait remboursement normal avec une probabilité de {}%'.format
+                            (id_input, round(predict_probability[0][0]*100 , 3)))
+  
+if __name__ == '__main__':
+    main()
 else:
-    st.session_state["page_control"] = 0
+    main1(client)
