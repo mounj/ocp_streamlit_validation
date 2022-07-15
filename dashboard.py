acdaeecd44d5052eb06924efd06a017f9b488b70
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-07-15 17:21:08 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -12,134 +12,26 @@ import io
 import plotly.express as px
 import plotly.graph_objs as go
 
-class State:
-    def __init__(self, path='state.pickle', default_state_class=dict):
-        self.path = path
-        self.default_state_class = default_state_class
+# Contents of ~/my_app/streamlit_app.py
+import streamlit as st
 
-    def load(self):
-        if os.path.exists(self.path):
-            with open(self.path, 'rb') as inf:
-                self.state = pickle.load(inf)
-        else:
-            self.state = self.default_state_class()
+def main_page():
+    st.markdown("# Main page 🎈")
+    st.sidebar.markdown("# Main page 🎈")
 
-    def get_state(self):
-        return self.state
+def page2():
+    st.markdown("# Page 2 ❄️")
+    st.sidebar.markdown("# Page 2 ❄️")
 
-    def save(self):
-        with open(self.path, 'wb') as outf:
-            pickle.dump(self.state, outf)
+def page3():
+    st.markdown("# Page 3 🎉")
+    st.sidebar.markdown("# Page 3 🎉")
 
-def rerun():
-    raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
+page_names_to_funcs = {
+    "Main Page": main_page,
+    "Page 2": page2,
+    "Page 3": page3,
+}
 
-def app():
-    store = State()
-    store.load()
-
-    name = store.get_state().get('name', None)
-    if name:
-        st.text(f'Hello {name}')
-    else:
-        ########################################################
-        # Loading images to the website
-        ########################################################
-        image = Image.open("images/credit.jpg")
-        
-        st.title('Bienvenue sur Octroi de crédit !')
-        current_path = os.getcwd()
-        credit_path = os.path.join(current_path, 'classifier.pkl')
-        with open(credit_path, 'rb') as handle:
-            model = pickle.load(handle)
-            
-        #@st.cache()
-        def prediction(X):
-            prediction = model.predict(X)
-            return prediction 
-        
-        def chargement_data(path):
-            dataframe = pd.read_csv(path)
-            liste_id = dataframe['SK_ID_CURR'].tolist()
-            return dataframe, liste_id
-        
-        examples_file = 'df1.csv'
-        dataframe, liste_id = chargement_data(examples_file)
-
-        st.image(image)
-        #st.sidebar.markdown("🛰️ **Navigation**")
-
-        id_input = st.selectbox(
-        'Choisissez le client que vous souhaitez visualiser',
-        liste_id)
-
-    
-        client_infos = dataframe[dataframe['SK_ID_CURR'] == id_input].drop(
-        ['SK_ID_CURR'], axis=1)
-        #client_infos = client_infos.to_dict('record')[0]
-        client_infos.to_dict(orient = 'records')
-    
-        result =""
-        if st.button("Predict"):
-        # correction 07/07 pour déceler les faux positifs
-            X1 = dataframe[dataframe['SK_ID_CURR'] == id_input]    
-            X = X1[['CODE_GENDER', 
-                    'AGE',
-                    'CNT_CHILDREN', 
-                    'DEF_30_CNT_SOCIAL_CIRCLE',
-                    'NAME_EDUCATION_TYPE_High education',  
-                    'NAME_EDUCATION_TYPE_Low education',  
-                    'NAME_EDUCATION_TYPE_Medium education',  
-                    'ORGANIZATION_TYPE_Construction',  
-                    'ORGANIZATION_TYPE_Electricity',  
-                    'ORGANIZATION_TYPE_Government/Industry',  
-                    'ORGANIZATION_TYPE_Medicine',  
-                    'ORGANIZATION_TYPE_Other/Construction/Agriculture',  
-                    'ORGANIZATION_TYPE_School',  
-                    'ORGANIZATION_TYPE_Services',  
-                    'ORGANIZATION_TYPE_Trade/Business', 
-                    'OCCUPATION_TYPE_Accountants/HR staff/Managers', 
-                    'OCCUPATION_TYPE_Core/Sales staff',  
-                    'OCCUPATION_TYPE_Laborers',  
-                    'OCCUPATION_TYPE_Medicine staff',  
-                    'OCCUPATION_TYPE_Private service staff' , 
-                    'OCCUPATION_TYPE_Tech Staff',
-                    'NAME_FAMILY_STATUS',
-                    'AMT_INCOME_TOTAL',
-                    'INCOME_CREDIT_PERC',
-                    'DAYS_EMPLOYED_PERC',
-                    'EXT_SOURCE_1',
-                    'EXT_SOURCE_2',    
-                    'EXT_SOURCE_3']]
-        
-            result = prediction(X)
-        
-            if  result == 1:
-              if int(X1['TARGET']) == 1: 
-                pred = 'Rejected (True Negative)'
-              else:
-                pred = 'Approved (False Negative)'
-            else:
-              if int(X1['TARGET']) == 1:
-                pred = 'Rejected (False Positive)'
-              else:
-                pred = 'Approved (True Positive)'              
-                   
-            st.success('Your loan is {}'.format(pred))
-        
-        
-        
-        st.text(f'Please enter your name')
-        name_input = st.text_input('your name')
-        name = name_input
-
-        if name != '':
-            store.get_state()['name'] = name
-
-        store.save()
-        next_page = st.button('Next page')
-        if next_page:
-            rerun()
-
-if __name__ == "__main__":
-    app()
+selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
+page_names_to_funcs[selected_page]()
