4d3e95ea0d2e1d8106d1bc8a421ab80238abeda1
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-07-15 17:37:11 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -27,11 +27,13 @@ def page3():
     st.markdown("# Page 3 🎉")
     st.sidebar.markdown("# Page 3 🎉")
 
-page_names_to_funcs = {
+my_dict = {
     "Main Page": main_page,
     "Page 2": page2,
     "Page 3": page3,
 }
 
-selected_page = st.sidebar.selectbox("Select a page", page_names_to_funcs.keys())
-page_names_to_funcs[selected_page]()
+keys = list(my_dict.keys())
+
+selected_page = st.sidebar.selectbox("Select a page", keys)
+my_dict[selected_page]()
