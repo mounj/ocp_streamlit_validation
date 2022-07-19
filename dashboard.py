4e5f423548735b004ec5e84cc855fdef47714c5f
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-07-19 17:35:23 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -259,51 +259,66 @@ def page3():
     NAME_EDUCATION_TYPE_Low_education , NAME_EDUCATION_TYPE_Medium_education , NAME_EDUCATION_TYPE_High_education = 0,0,0
     if NAME_EDUCATION_TYPE == 'Low education':
          NAME_EDUCATION_TYPE_Low_education = 1
+         X2['NAME_EDUCATION_TYPE_Low education'] = 1   
     elif NAME_EDUCATION_TYPE == 'Medium education':
          NAME_EDUCATION_TYPE_Medium_education = 1
+         X2['NAME_EDUCATION_TYPE_Medium education'] = 1       
     else:
          NAME_EDUCATION_TYPE_High_education = 1
-    X2['NAME_EDUCATION_TYPE'] = NAME_EDUCATION_TYPE    
-            
+         X2['NAME_EDUCATION_TYPE_High education']   = 1
+     
     ORGANIZATION_TYPE_Construction, ORGANIZATION_TYPE_Electricity, ORGANIZATION_TYPE_Government_Industry = 0,0,0
     ORGANIZATION_TYPE_Medicine, ORGANIZATION_TYPE_Other_Construction_Agriculture, ORGANIZATION_TYPE_School = 0,0,0
     ORGANIZATION_TYPE_Services, ORGANIZATION_TYPE_Trade_Business = 0,0 
     if ORGANIZATION_TYPE == 'Construction':
               ORGANIZATION_TYPE_Construction = 1
+              X2['ORGANIZATION_TYPE_Construction'] = 1
     elif ORGANIZATION_TYPE == 'Electricity':
                ORGANIZATION_TYPE_Electricity = 1
+               X2['ORGANIZATION_TYPE_Electricity'] = 1
     elif ORGANIZATION_TYPE ==  'Government/Industry':   
                ORGANIZATION_TYPE_Government_Industry = 1 
+               X2['ORGANIZATION_TYPE_Government/Industry'] = 1
     elif  ORGANIZATION_TYPE == 'Medicine':    
                ORGANIZATION_TYPE_Medicine = 1
+               X2['ORGANIZATION_TYPE_Medicine'] = 1
     elif  ORGANIZATION_TYPE == 'Other/Construction/Agriculture':
                ORGANIZATION_TYPE_Other_Construction_Agriculture = 1
+               X2['ORGANIZATION_TYPE_Other/Construction/Agriculture'] = 1 
     elif  ORGANIZATION_TYPE ==  'School': 
                ORGANIZATION_TYPE_School = 1
+               X2['ORGANIZATION_TYPE_School'] = 1
     elif ORGANIZATION_TYPE == 'Services':
                ORGANIZATION_TYPE_Services = 1
+               X2['ORGANIZATION_TYPE_Services'] = 1
     elif ORGANIZATION_TYPE == 'Trade/Business':
                ORGANIZATION_TYPE_Trade_Business = 1
-    X2['ORGANIZATION_TYPE'] = ORGANIZATION_TYPE    
-            
+               X2['ORGANIZATION_TYPE_Trade/Business'] = 1
+               
     OCCUPATION_TYPE_Accountants_HR_staff_Managers, OCCUPATION_TYPE_Core_Sales_staff, OCCUPATION_TYPE_Laborers = 0,0,0  
     OCCUPATION_TYPE_Medicine_staff, OCCUPATION_TYPE_Private_service_staff, OCCUPATION_TYPE_Tech_Staff = 0,0,0
     if OCCUPATION_TYPE == 'Accountants/HR staff/Managers':
                OCCUPATION_TYPE_Accountants_HR_staff_Managers = 1
+               X2['OCCUPATION_TYPE_Accountants/HR staff/Managers'] = 1
     elif OCCUPATION_TYPE == 'Core/Sales staff':
                OCCUPATION_TYPE_Core_Sales_staff = 1
+               X2['OCCUPATION_TYPE_Core/Sales staff'] = 1
     elif OCCUPATION_TYPE == 'Laborers':
                OCCUPATION_TYPE_Laborers = 1
+               X2['OCCUPATION_TYPE_Laborers'] = 1
     elif OCCUPATION_TYPE == 'Medicine staff':
                OCCUPATION_TYPE_Medicine_staff = 1
+               X2['OCCUPATION_TYPE_Medicine staff'] = 1
     elif OCCUPATION_TYPE == 'Private service staff':
                OCCUPATION_TYPE_Private_service_staff = 1 
+               X2['OCCUPATION_TYPE_Private service staff'] = 1
     elif OCCUPATION_TYPE ==  'Tech Staff':
                OCCUPATION_TYPE_Tech_Staff = 1
-    X2['OCCUPATION_TYPE'] = OCCUPATION_TYPE
-        
+               X2['OCCUPATION_TYPE_Tech Staff'] = 1
+       
     NAME_FAMILY_STATUS = 0 if  NAME_FAMILY_STATUS == 'Single' else 1
     X2['NAME_FAMILY_STATUS'] = NAME_FAMILY_STATUS
+    
     transparence = prediction(X2)
     predict_probability = model.predict_proba(X2)
         
