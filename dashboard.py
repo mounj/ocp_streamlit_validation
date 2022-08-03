cd33852ed29cd4ad6b9466b49fb2587ff730c317
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-08-03 22:22:58 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -7,13 +7,13 @@ import json
 import pickle
 import os
 import shap
-from PIL import Image
 from sklearn.preprocessing import StandardScaler
 import io
 import plotly.express as px
 import plotly.graph_objs as go
 import streamlit.components.v1 as components
 import xgboost as xgb
+import Image
 
 # Chargement du modèle
 #current_path = os.getcwd()
