6a7d4f98080dec3066309df73b0ade773650f724
Brice893 <92929586+Brice893@users.noreply.github.com>
2022-08-08 14:13:12 +0200

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -215,54 +215,14 @@ def page2():
           'EXT_SOURCE_2',    
           'EXT_SOURCE_3']]
      
-    dataf = dataframe[['CODE_GENDER', 
-        'AGE',
-        'CNT_CHILDREN', 
-        'DEF_30_CNT_SOCIAL_CIRCLE',
-         'NAME_EDUCATION_TYPE_High education',  
-         'NAME_EDUCATION_TYPE_Low education',  
-         'NAME_EDUCATION_TYPE_Medium education',  
-         'ORGANIZATION_TYPE_Construction',  
-         'ORGANIZATION_TYPE_Electricity',  
-         'ORGANIZATION_TYPE_Government/Industry',  
-         'ORGANIZATION_TYPE_Medicine',  
-         'ORGANIZATION_TYPE_Other/Construction/Agriculture',  
-         'ORGANIZATION_TYPE_School',  
-         'ORGANIZATION_TYPE_Services',  
-         'ORGANIZATION_TYPE_Trade/Business', 
-         'OCCUPATION_TYPE_Accountants/HR staff/Managers', 
-         'OCCUPATION_TYPE_Core/Sales staff',  
-         'OCCUPATION_TYPE_Laborers',  
-         'OCCUPATION_TYPE_Medicine staff',  
-         'OCCUPATION_TYPE_Private service staff' , 
-         'OCCUPATION_TYPE_Tech Staff',
-         'NAME_FAMILY_STATUS_Married',
-         'NAME_FAMILY_STATUS_Single',  
-          'AMT_INCOME_TOTAL',
-          'INCOME_CREDIT_PERC',
-          'DAYS_EMPLOYED_PERC',
-          'EXT_SOURCE_1',
-          'EXT_SOURCE_2',    
-          'EXT_SOURCE_3']]
-           
-    
+       
     
     # Variables globales
     st.header('Variables globales du modèle XGBOOST :')        
-    #feat_importances = pd.Series(model.feature_importances_, index=X.columns).sort_values(ascending=False)
-    #impPlot(feat_importances, 'XGBOOST Classifier') 
+    feat_importances = pd.Series(model.feature_importances_, index=X.columns).sort_values(ascending=False)
+    impPlot(feat_importances, 'XGBOOST Classifier') 
     
-    explainer = shap.Explainer(model, dataf)
-    shap_values = explainer(dataf)
-
-    #st_shap(shap.plots.waterfall(shap_values[0]), height=300)
-    #st_shap(shap.plots.beeswarm(shap_values), height=300)
-
-    explainer = shap.TreeExplainer(model)
-    shap_values = explainer.shap_values(dataf)
-    
-    st_shap(shap.summary_plot(shap_values, dataf, plot_type="bar"))
-    
+       
     # Variables locales
     st.header('Variables locales du modèle XGBOOST :')
     # compute SHAP values
